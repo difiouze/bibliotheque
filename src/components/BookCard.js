5f9031057cde40ad8d4a1ddd59636402400670b4
Laurent RAVIX <laurentravix@gmail.com>
2026-03-25 21:26:58 +0100

    feat: BookCard.js

---
+++ src/components/BookCard.js
@@ -0,0 +1,9 @@
+const BookCard = ({ book }) => {
+    return (
+    <div className="book-card">
+        <h3>{book.title}</h3>
+    </div>
+    )
+}
+
+export default BookCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,45 @@
 import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
+
+// components
+import BookCard from "../components/BookCard";
 
 const Home = () => {
-  console.log(supabase)
+  const [fetchError, setFectchError] = useState(null);
+  const [books, setBooks] = useState(null);
+
+  useEffect(()=> {
+    const fetchBooks = async () => {
+      const { data, error } = await supabase
+        .from('livres')
+        .select()
+
+        if(error) {
+          setFectchError('Livres non trouvés')
+          setBooks(null)
+          console.log(error)
+        }
+
+        if(data) {
+          setBooks(data)
+          setFectchError(null)
+        }
+    }
+    fetchBooks()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {books && (
+       <div className="books">
+         <div className="books-grid">
+          {books.map(book => (
+            <BookCard key={book.id} book={book}/>
+          ))}
+        </div>
+       </div> 
+      )}
     </div>
   )
 }
