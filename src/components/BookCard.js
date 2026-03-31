7ee4f60d798039e5a22095128b5c55fff38ab382
Laurent RAVIX <laurentravix@gmail.com>
2026-03-31 14:42:02 +0200

    feat: fetch single record

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BookCard = ({ book }) => {
     return (
     <div className="book-card">
         <h3>{book.title}</h3>
         <span>{book.collection}</span>
+        <div className="button">
+            <Link to={'/' + book.id}>
+                <i className="material-icons">edit</i>
+            </Link>
+        </div>
     </div>
     )
 }
